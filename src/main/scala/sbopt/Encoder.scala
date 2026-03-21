3ccb5c0abf66b8c8e213a3fc57842c972312b3d3
Yann Moisan <yann@zen.ly>
2026-03-21 08:41:07 +0100

    fix: add explicit return types to all implicit definitions
    Suppresses -Xlint warning "Implicit definition should have explicit type"
    for all implicit vals and defs in Encoder companion object.

--- src/main/scala/sbopt/Encoder.scala
+++ src/main/scala/sbopt/Encoder.scala
@@ -8,31 +8,31 @@ trait Encoder[A] {
 }
 
 object Encoder {
-  implicit val stringEncoder = new Encoder[String] {
+  implicit val stringEncoder: Encoder[String] = new Encoder[String] {
     override def encode(a: String): String = a
   }
-  implicit val intEncoder = new Encoder[Int] {
+  implicit val intEncoder: Encoder[Int] = new Encoder[Int] {
     override def encode(a: Int): String = String.valueOf(a)
   }
-  implicit val longEncoder = new Encoder[Long] {
+  implicit val longEncoder: Encoder[Long] = new Encoder[Long] {
     override def encode(a: Long): String = String.valueOf(a)
   }
-  implicit val floatEncoder = new Encoder[Float] {
+  implicit val floatEncoder: Encoder[Float] = new Encoder[Float] {
     override def encode(a: Float): String = String.valueOf(a)
   }
-  implicit val doubleEncoder = new Encoder[Double] {
+  implicit val doubleEncoder: Encoder[Double] = new Encoder[Double] {
     override def encode(a: Double): String = String.valueOf(a)
   }
-  implicit val booleanEncoder = new Encoder[Boolean] {
+  implicit val booleanEncoder: Encoder[Boolean] = new Encoder[Boolean] {
     override def encode(a: Boolean): String = String.valueOf(a)
   }
-  implicit val fileEncoder = new Encoder[File] {
+  implicit val fileEncoder: Encoder[File] = new Encoder[File] {
     override def encode(a: File): String = a.getAbsolutePath()
   }
-  implicit val pathEncoder = new Encoder[Path] {
+  implicit val pathEncoder: Encoder[Path] = new Encoder[Path] {
     override def encode(a: Path): String = a.toAbsolutePath().toString
   }
-  implicit def tuple2Encoder[A: Encoder, B: Encoder] =
+  implicit def tuple2Encoder[A: Encoder, B: Encoder]: Encoder[(A, B)] =
     new Encoder[Tuple2[A, B]] {
       override def encode(a: (A, B)): String = {
         val encoderA = implicitly[Encoder[A]]
@@ -40,32 +40,32 @@ object Encoder {
         s"${encoderA.encode(a._1)}=${encoderB.encode(a._2)}"
       }
     }
-  implicit def optionEncoder[A: Encoder] =
+  implicit def optionEncoder[A: Encoder]: Encoder[Option[A]] =
     new Encoder[Option[A]] {
       override def encode(a: Option[A]): String = {
         val encoderA = implicitly[Encoder[A]]
         a.fold("")(encoderA.encode)
       }
     }
-  implicit def someEncoder[A: Encoder] =
+  implicit def someEncoder[A: Encoder]: Encoder[Some[A]] =
     new Encoder[Some[A]] {
       override def encode(a: Some[A]): String = {
         val encoderA = implicitly[Encoder[A]]
         encoderA.encode(a.get)
       }
     }
-  implicit def noneEncoder =
+  implicit def noneEncoder: Encoder[None.type] =
     new Encoder[None.type] {
       override def encode(a: None.type): String = ""
     }
-  implicit def listEncoder[A: Encoder] =
+  implicit def listEncoder[A: Encoder]: Encoder[List[A]] =
     new Encoder[List[A]] {
       override def encode(a: List[A]): String = {
         val encoder = implicitly[Encoder[A]]
         a.map(encoder.encode).mkString(",")
       }
     }
-  implicit def from[A, B: Encoder](f: A => B) =
+  implicit def from[A, B: Encoder](f: A => B): Encoder[A] =
     new Encoder[A] {
       override def encode(a: A): String = {
         val encoder = implicitly[Encoder[B]]
